7c65b60a20946fa5d0ee902508810516e353f9cb
Johnson Earls <johnson.earls@oracle.com>
2018-12-05 07:40:21 -0800

    Refactor day 1

--- day_01.py
+++ day_01.py
@@ -1,15 +1,18 @@
 
 import sys
 
-def day_01a(input_data):
-    return sum(int(freq_adjust) for freq_adjust in input_data.strip().split())
+def process_input_data(input_data):
+    return input_data.strip().split()
 
-def day_01b(input_data):
+def part_1(input_data):
+    return sum(int(freq_adjust) for freq_adjust in input_data)
+
+def part_2(input_data):
     frequency = 0
     seen = set()
     freq_dup = None
     while freq_dup is None:
-        input_list = (int(frequency) for frequency in input_data.strip().split())
+        input_list = (int(frequency) for frequency in input_data)
         for freq_adjust in input_list:
             frequency += freq_adjust
             if frequency in seen:
@@ -18,7 +21,14 @@ def day_01b(input_data):
             seen.add(frequency)
     return freq_dup
 
-input_data = open('day_01.input').read()
+if __name__ == '__main__':
 
-print "Day 01 a = {}".format(day_01a(input_data))
-print "Day 01 b = {}".format(day_01b(input_data))
+    sample_data = process_input_data(open(__file__.rsplit('.')[0] + '.sample').read())
+
+    print "sample data: part 1 = {}".format(part_1(sample_data))
+    print "sample data: part 2 = {}".format(part_2(sample_data))
+
+    input_data = process_input_data(open(__file__.rsplit('.')[0] + '.input').read())
+
+    print "real input: part 1 = {}".format(part_1(input_data))
+    print "real input: part 2 = {}".format(part_2(input_data))
