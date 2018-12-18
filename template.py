bf7a3d3e169ca67e2a916bd44161e931d572f262
Johnson Earls <johnson.earls@oracle.com>
2018-12-18 06:35:21 -0800

    Rework template again

--- template.py
+++ template.py
@@ -8,51 +8,47 @@ import os.path
 
 DEBUGGING = False
 
+# pull in python-native version of `accumulate` from python 3
+def accumulate(iterable, func=operator.add):
+    'Return running totals'
+    # accumulate([1,2,3,4,5]) --> 1 3 6 10 15
+    # accumulate([1,2,3,4,5], operator.mul) --> 1 2 6 24 120
+    it = iter(iterable)
+    try:
+        total = next(it)
+    except StopIteration:
+        return
+    yield total
+    for element in it:
+        total = func(total, element)
+        yield total
+
 if DEBUGGING:
 
-    def reduce(func, iterable, initial=None):
-        iterable = iter(iterable)
-        if initial is None:
-            initial = iterable.next()
-        for value in iterable:
-            print >> sys.stderr, ">>reduce<< state={0!r} value={1!r}".format(initial, value)
-            initial = func(initial, value)
-        print >> sys.stderr, ">>reduce<< final state={0!r}".format(initial)
-        return initial
+    print >> sys.stderr, "Debugging version of `reduce` enabled"
 
-    # pull in python-native version of `accumulate` from python 3
-    def accumulate(iterable, func=operator.add):
-        'Return running totals'
-        # accumulate([1,2,3,4,5]) --> 1 3 6 10 15
-        # accumulate([1,2,3,4,5], operator.mul) --> 1 2 6 24 120
-        it = iter(iterable)
-        try:
-            total = next(it)
-        except StopIteration:
-            return
-        print >> sys.stderr, ">>accumulate<< initial state={0!r}".format(total)
-        yield total
-        for element in it:
-            total = func(total, element)
-            print >> sys.stderr, ">>accumulate<< state={0!r}".format(total)
-            yield total
+    real_reduce = reduce
+    def reduce(func, *args):
+        def debug(state, value):
+            print >> sys.stderr, ">>reduce<< state={!r} value={!r}".format(state, value)
+            return func(state, value)
+        final = real_reduce(debug, *args)
+        print >> sys.stderr, ">>reduce<< final state={0!r}".format(final)
+        return final
 
-else:
+if DEBUGGING:
 
-    # pull in python-native version of `accumulate` from python 3
-    def accumulate(iterable, func=operator.add):
-        'Return running totals'
-        # accumulate([1,2,3,4,5]) --> 1 3 6 10 15
-        # accumulate([1,2,3,4,5], operator.mul) --> 1 2 6 24 120
-        it = iter(iterable)
-        try:
-            total = next(it)
-        except StopIteration:
-            return
-        yield total
-        for element in it:
-            total = func(total, element)
-            yield total
+    print >> sys.stderr, "Debugging version of `accumulate` enabled"
+
+    real_accumulate = accumulate
+    def accumulate(iterable, func = operator.add):
+        def debug(state, value):
+            print >> sys.stderr, ">>accumulate<< element={!r}".format(value)
+            return func(state, value)
+        for state in real_accumulate(iterable, debug):
+            print >> sys.stderr, ">>accumulate<< state={!r}".format(state)
+            yield state
+        print >> sys.stderr, ">>accumulate<< done"
 
 ########################################################################
 #
@@ -87,12 +83,12 @@ if __name__ == '__main__':
     base_filename = __file__.rsplit('.')[0]
 
     input_filename = '{}.input'.format(base_filename)
-    input_data = process_input_data(open(input_filename).read())
+    input_data = open(input_filename).read()
 
     for sample_num in itertools.takewhile(lambda num:os.path.exists('{}.sample.{}'.format(base_filename, num)), itertools.count(1)):
 
         sample_filename = '{}.sample.{}'.format(base_filename, sample_num)
-        sample_data = process_input_data(open(sample_filename).read())
+        sample_data = open(sample_filename).read()
 
         results_filename = '{}.sample.results.part1.{}'.format(base_filename, sample_num)
         expected = (
@@ -102,21 +98,19 @@ if __name__ == '__main__':
                 )
 
         t = time.time()
-        result = part_1(sample_data)
+        result = part_1(process_input_data(sample_data))
         t = time.time() - t
         print "{}: sample {}: part 1 = {}{}".format(t, sample_num, result, " (expected {})".format(expected) if result != expected else "")
 
-        if DEBUGGING: sys.exit(0)
-
     t = time.time()
-    result = part_1(input_data)
+    result = part_1(process_input_data(input_data))
     t = time.time() - t
     print "{}: input data: part 1 = {}".format(t, result)
 
     for sample_num in itertools.takewhile(lambda num:os.path.exists('{}.sample.{}'.format(base_filename, num)), itertools.count(1)):
 
         sample_filename = '{}.sample.{}'.format(base_filename, sample_num)
-        sample_data = process_input_data(open(sample_filename).read())
+        sample_data = open(sample_filename).read()
 
         results_filename = '{}.sample.results.part2.{}'.format(base_filename, sample_num)
         expected = (
@@ -126,14 +120,12 @@ if __name__ == '__main__':
                 )
 
         t = time.time()
-        result = part_2(sample_data)
+        result = part_2(process_input_data(sample_data))
         t = time.time() - t
         print "{}: sample {}: part 2 = {}{}".format(t, sample_num, result, " (expected {})".format(expected) if result != expected else "")
 
-        if DEBUGGING: sys.exit(0)
-
     t = time.time()
-    result = part_2(input_data)
+    result = part_2(process_input_data(input_data))
     t = time.time() - t
     print "{}: input data: part 2 = {}".format(t, result)
 
